73af821ced44792e309a6a5139b6cd786d0018a7
GiovanniKaaijk <kaaijkg@gmail.com>
2020-01-23 12:01:45 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thorikos/precache-manifest.55282a38668ba5819b2d50c0829ba336.js"
+  "/thorikos/precache-manifest.e17370d03d69b8187fd004e27c963b0c.js"
 );
 
 self.addEventListener('message', (event) => {
